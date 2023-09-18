9c5847485439184e13e49e475a94f0401dca4f54
Andre Zeps <andre.zeps@googlemail.com>
2023-09-18 16:41:32 +0200

    WIP

--- demo/unity.c
+++ demo/unity.c
@@ -17,75 +17,91 @@
 
 #include "unity.h"
 
-static Music_Emu* emu=NULL;
+static Music_Emu *emu = NULL;
 static float volume = 1.0f;
 static short rawSampleData[4096];
-static FILE* debugOut=NULL;
+static FILE *debugOut = NULL;
 
-static gme_equalizer_t gme_equalizer_state={0};
+static gme_equalizer_t gme_equalizer_state = {0};
 
-const char* NsfOpenFile(const char* path, int sampleRate)
+const char *NsfOpenFile(const char *path, int sampleRate)
 {
 	return gme_open_file(path, &emu, sampleRate);
 }
 
-int NsfActivateDebugOut(const char* path)
+int NsfActivateDebugOut(const char *path)
 {
-	debugOut = fopen("/tmp/gmeunity.raw","wb");
+	debugOut = fopen("/tmp/gmeunity.raw", "wb");
 	return (debugOut != NULL);
 }
 
-const char* NsfOpenData(uint8_t* data, int dataLen, int sampleRate)
+const char *NsfOpenData(uint8_t *data, int dataLen, int sampleRate)
 {
-	return gme_open_data(data,dataLen,&emu,sampleRate);
+	return gme_open_data(data, dataLen, &emu, sampleRate);
 }
 
-const char* NsfPlayTrack(int index) {
+const char *NsfPlayTrack(int index)
+{
 	if (emu)
+	{
+		gme_info_t *info;
+		gme_err_t err = gme_track_info(emu, &info, index);
+		if (err){
+			printf("Err %d\n",err);
+		}
+
+		assert(info);
+		fprintf(stderr,"Song: %s\n",info->song);
+		fprintf(stderr,"System: %s\n",info->system);
+		fprintf(stderr,"Lengths: %d %d %d\n",info->length, info->intro_length, info->loop_length);
+
 		return gme_start_track(emu, index);
+	}
 	else
 		return NULL;
 }
 
-const char* NsfGetSamples(int numSamples, float* sampleData, int channels)
+const char *NsfGetSamples(int numSamples, float *sampleData, int channels)
 {
-	const char* err="FileNotOpened";
+	const char *err = "FileNotOpened";
 	if (emu)
 	{
 		err = gme_play(emu, numSamples, rawSampleData);
-		int outIndex=0;
+		int outIndex = 0;
 
 		if (err == NULL)
 		{
-			for (int i =0;i< numSamples; i++)
+			for (int i = 0; i < numSamples; i++)
 			{
-				for (int j =0;j< channels; j++)
+				for (int j = 0; j < channels; j++)
 				{
-					sampleData[outIndex++]=rawSampleData[i]*volume/((float)SHRT_MAX);
+					sampleData[outIndex++] = rawSampleData[i] * volume / ((float)SHRT_MAX);
 				}
 			}
 		}
 
 		if (debugOut)
 		{
-			fwrite(sampleData,sizeof(float),outIndex,debugOut);
+			fwrite(sampleData, sizeof(float), outIndex, debugOut);
 		}
 	}
 
 	return err;
 }
 
-void NsfSetVolume(float val) {
+void NsfSetVolume(float val)
+{
 	volume = val;
 }
 
-void NsfClose() {
+void NsfClose()
+{
 	if (emu)
 	{
 		if (debugOut)
 		{
 			fclose(debugOut);
-			debugOut=NULL;
+			debugOut = NULL;
 		}
 		gme_delete(emu);
 		emu = NULL;
@@ -106,25 +122,60 @@ void NsfSetTempo(double tempo)
 
 void NsfSetEqualizer(double treble, double bass)
 {
-	gme_equalizer_state.bass=bass;
-	gme_equalizer_state.treble=treble;
+	gme_equalizer_state.bass = bass;
+	gme_equalizer_state.treble = treble;
 	if (emu)
-		gme_set_equalizer(emu,&gme_equalizer_state);
+		gme_set_equalizer(emu, &gme_equalizer_state);
 }
 
 double NsfGetEqualizerTreble()
 {
 	if (emu)
-		gme_equalizer(emu,&gme_equalizer_state);
+		gme_equalizer(emu, &gme_equalizer_state);
 	return gme_equalizer_state.treble;
 }
 
 double NsfGetEqualizerBass()
 {
 	if (emu)
-		gme_equalizer(emu,&gme_equalizer_state);
+		gme_equalizer(emu, &gme_equalizer_state);
 	return gme_equalizer_state.bass;
 }
 
+/**
+ * @brief Gets current position in played samples
+ *
+ * @return int position as played samples
+ */
+int NsfGetCurrentSamplePosition()
+{
+	if (emu)
+		return gme_tell_samples(emu);
 
+	return -1;
+}
 
+void NsfSeekSamplePosition(int samples)
+{
+	if (emu)
+		gme_seek_samples(emu, samples);
+}
+
+/**
+ * @brief Gets current position in played milliseconds
+ *
+ * @return int played milliseconds since track start
+ */
+int NsfGetCurrentTimePosition()
+{
+	if (emu)
+		return gme_tell(emu);
+
+	return -1;
+}
+
+void NsfSeekTimePosition(int milliseconds)
+{
+	if (emu)
+		gme_seek(emu, milliseconds);
+}
--- demo/unitytest.c
+++ demo/unitytest.c
@@ -5,13 +5,12 @@
  *      Author: andre
  */
 
-
 #include "unity.h"
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-uint8_t* dump_file(const char*file_path, size_t *size)
+uint8_t *dump_file(const char *file_path, size_t *size)
 {
 	FILE *in = fopen(file_path, "rb");
 	uint8_t *buffer = NULL;
@@ -20,7 +19,7 @@ uint8_t* dump_file(const char*file_path, size_t *size)
 	fseek(in, 0, SEEK_END);
 	(*size) = (size_t)ftell(in);
 	fseek(in, 0, SEEK_SET);
-	buffer = (uint8_t*)malloc(*size);
+	buffer = (uint8_t *)malloc(*size);
 	fread(buffer, 1, *size, in);
 	return buffer;
 }
@@ -28,30 +27,30 @@ uint8_t* dump_file(const char*file_path, size_t *size)
 int main(int argc, char *argv[])
 {
 	const char *filename = "test.nsf"; /* Default file to open */
-	if ( argc >= 2 )
+	if (argc >= 2)
 		filename = argv[1];
 
 	int sample_rate = 44100; /* number of samples per second */
 	/* index of track to play (0 = first) */
 	int track = argc >= 3 ? atoi(argv[2]) : 0;
 
-	const char* err;
+	const char *err;
 #if 0
 	err = NsfOpenFile(filename, sample_rate);
 	assert(!err);
 #else
 	size_t file_size = 0;
 	uint8_t *file_data = dump_file(filename, &file_size);
-	err=NsfOpenData(file_data, file_size, sample_rate);
+	err = NsfOpenData(file_data, file_size, sample_rate);
 	assert(!err);
 #endif
 
-	fprintf(stderr, "bass=%f, treble=%f\n",NsfGetEqualizerBass(), NsfGetEqualizerTreble());
+	fprintf(stderr, "bass=%f, treble=%f\n", NsfGetEqualizerBass(), NsfGetEqualizerTreble());
 
 	err = NsfPlayTrack(track);
 	assert(!err);
 
-	fprintf(stderr, "bass=%f, treble=%f\n",NsfGetEqualizerBass(), NsfGetEqualizerTreble());
+	fprintf(stderr, "bass=%f, treble=%f\n", NsfGetEqualizerBass(), NsfGetEqualizerTreble());
 
 	// double treble; /* -50.0 = muffled, 0 = flat, +5.0 = extra-crisp */
 	// double bass;   /* 1 = full bass, 90 = average, 16000 = almost no bass */
@@ -59,22 +58,21 @@ int main(int argc, char *argv[])
 	double treble = NsfGetEqualizerTreble();
 	double bass = NsfGetEqualizerBass();
 	NsfSetVolume(0.4);
-	NsfSetTempo(3.5);
-	for(;;)
+	// NsfSetTempo(3.5);
+	for (;;)
 	{
 		float samples[1024];
 
-		err = NsfGetSamples(1024, samples,1);
+		err = NsfGetSamples(1024, samples, 1);
 		if (err)
 			puts(err);
 
 		assert(!err);
-		fwrite(samples,1,1024*4,stdout);
+		fwrite(samples, 1, 1024 * 4, stdout);
 
-		//treble-=0.2;
-		//bass+=10;
+		// treble-=0.2;
+		// bass+=10;
 		NsfSetEqualizer(treble, bass);
-
 	}
 
 	return 0;
